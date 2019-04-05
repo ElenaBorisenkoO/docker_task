4f8183fe8ca2ea896020ccb47a00c8c7ec1520da
Lena <elena.borisenko1991@gmail.com>
2019-04-05 00:43:26 +0000

    add new config

--- start.sh
+++ start.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
-git pull
-#docker stop docker-app
-#docker rm docker-app
-#docker rmi docker-static
+#git pull
+docker stop docker-app
+docker rm docker-app
+docker rmi docker-static
 docker build -t docker-static .
-#docker run --name docker-app -p 3000:90 docker-static
-docker run -p 3000:90 docker-static
+docker run --name docker-app -p 3000:90 docker-static
+#docker run -p 3000:90 docker-static
 
